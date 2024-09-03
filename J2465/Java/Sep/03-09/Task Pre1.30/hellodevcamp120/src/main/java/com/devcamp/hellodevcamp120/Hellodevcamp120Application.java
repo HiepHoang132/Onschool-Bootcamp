48327f904a3fcdf1615bf8c8d0fde94a33ea017d
Hiep Hoang <85355255+HiepHoang132@users.noreply.github.com>
2024-09-03 15:09:51 +0700

    Add folder Java 03-09

---
+++ J2465/Java/Sep/03-09/Task Pre1.30/hellodevcamp120/src/main/java/com/devcamp/hellodevcamp120/Hellodevcamp120Application.java
@@ -0,0 +1,13 @@
+package com.devcamp.hellodevcamp120;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hellodevcamp120Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hellodevcamp120Application.class, args);
+	}
+
+}
---
+++ J2465/Java/Sep/03-09/Task Pre1.30/hellodevcamp120/src/test/java/com/devcamp/hellodevcamp120/Hellodevcamp120ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devcamp.hellodevcamp120;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hellodevcamp120ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
