87ea648c9a23823d6fcb4f3f5c09764ef07d9cdc
Hiep Hoang <85355255+HiepHoang132@users.noreply.github.com>
2024-09-05 23:38:31 +0700

    Add folder JS 29-08

--- J2465/JS/Aug/29-08/src/index.js
+++ J2465/JS/Aug/29-08/src/index.js
@@ -1,26 +1,35 @@
 import "./styles/main.scss";
 
-document.addEventListener("DOMContentLoaded", function () {
-  var sidebar = document.getElementById("sidebar");
-  var sidebarToggles = document.querySelectorAll(".sidebar-toggle");
-  var closeBtn = document.getElementById("close-btn");
-  var overlay = document.getElementById("overlay");
+("use strict");
 
-  // Loop through all toggle buttons and add click event
-  sidebarToggles.forEach(function (toggle) {
-    toggle.addEventListener("click", function () {
-      sidebar.classList.add("active");
-      overlay.classList.add("active");
-    });
-  });
 
-  closeBtn.addEventListener("click", function () {
-    sidebar.classList.remove("active");
-    overlay.classList.remove("active");
-  });
+const elemToggleFunc = function (elem) {
+  elem.classList.toggle("active");
+};
 
-  overlay.addEventListener("click", function () {
-    sidebar.classList.remove("active");
-    overlay.classList.remove("active");
+const navbar = document.querySelector("[data-navbar]");
+const overlay = document.querySelector("[data-overlay]");
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+
+const navElemArr = [overlay, navCloseBtn, navOpenBtn];
+
+for (let i = 0; i < navbarLinks.length; i++) {
+  navElemArr.push(navbarLinks[i]);
+}
+
+for (let i = 0; i < navElemArr.length; i++) {
+  navElemArr[i].addEventListener("click", function () {
+    elemToggleFunc(navbar);
+    elemToggleFunc(overlay);
   });
+}
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+  window.scrollY >= 400
+    ? header.classList.add("active")
+    : header.classList.remove("active");
 });
